8432efb53057a9b34b56e5c8caaef7c712482f2a
Lucaus <mzx2602685411@gmail.com>
2024-01-16 21:41:55 +0800

    refactor(route): 📦 redirect / to /home/discover

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/home/discover');
+  }, [navigate]);
+
   return (
     <>
       <Outlet></Outlet>
