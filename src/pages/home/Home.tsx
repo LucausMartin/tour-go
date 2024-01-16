ac8a7c8ca49be5bf5dfdaf6a1ddd725fabfb75a7
Lucaus <mzx2602685411@gmail.com>
2024-01-16 21:47:22 +0800

    feat(home page): ✨ add theme color and routing adaptation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,14 @@
 import { FC } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './home.css';
-import { useNavigate } from 'react-router-dom';
 import { NavigateBar } from './components/Navigate/Navigate.tsx';
-import { Button } from '@mui/material';
 
 export const Home: FC = () => {
-  const navigate = useNavigate();
-  const navToLogin = () => {
-    navigate('/login');
-  };
   return (
     <div className="home-container">
       <NavigateBar />
       <main className="home-main">
-        <Button onClick={navToLogin}>Login</Button>
+        <Outlet></Outlet>
       </main>
     </div>
   );
