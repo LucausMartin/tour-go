e7a43c49d9b4d601705e9417a8c67276a63a758c
Lucaus <mzx2602685411@gmail.com>
2024-01-16 21:43:05 +0800

    feat(error page): ✨ add error img for error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react';
 import './error.css';
+import ErrorImage from '../../assets/404.png';
 
 const Error: FC = () => {
-  return <div className="error-container"></div>;
+  return (
+    <div className="error-container">
+      <img src={ErrorImage} alt="404" />
+    </div>
+  );
 };
 
 export { Error };
