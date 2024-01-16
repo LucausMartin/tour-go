7b2ca5d3195dbce7a006d33ccbeb267888798e98
Lucaus <mzx2602685411@gmail.com>
2024-01-16 21:37:07 +0800

    refactor(route): 📦 refactor routes config

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter, createRoutesFromElements, Route } from 'react-rout
 import App from './App.tsx';
 import { Home } from './pages/home/Home.tsx';
 import { Error } from './pages/error/Error.tsx';
+import { Discover } from './pages/home/pages/discover/Discover.tsx';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<Error />}>
-      <Route index element={<Home />} />
+      <Route path="home" element={<Home />}>
+        <Route caseSensitive path="discover" element={<Discover />}></Route>
+        <Route caseSensitive path="me" lazy={() => import('./pages/home/pages/me/Me.tsx')} />
+        <Route caseSensitive path="publish" lazy={() => import('./pages/home/pages/publish/Publish.tsx')} />
+      </Route>
       <Route caseSensitive path="login" lazy={() => import('./pages/login/Login.tsx')} />
       <Route caseSensitive path="register" lazy={() => import('./pages/register/Register.tsx')} />
     </Route>
