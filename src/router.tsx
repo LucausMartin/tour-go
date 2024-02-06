299dc12785c9cc789049f764122c4aaffe7c1507
Lucaus <mzx2602685411@gmail.com>
2024-02-06 18:03:40 +0800

    feat(router): ✨ delete useless route

--- src/router.tsx
+++ src/router.tsx
@@ -10,7 +10,7 @@ export const router = createBrowserRouter(
     <Route path="/" element={<App />} errorElement={<Error />}>
       <Route caseSensitive path="home" element={<Home />}>
         <Route caseSensitive path="discover" element={<Discover />}>
-          <Route caseSensitive path=":kind" element={<DiscoverContent />} />
+          <Route caseSensitive path=":kind" element={<DiscoverContent />}></Route>
         </Route>
         <Route caseSensitive path="me" lazy={() => import('./pages/home/pages/me/Me.tsx')}>
           <Route caseSensitive path=":kind" element={<MeContent />} />
@@ -18,8 +18,6 @@ export const router = createBrowserRouter(
         <Route caseSensitive path="publish" lazy={() => import('./pages/home/pages/publish/Publish.tsx')} />
         <Route caseSensitive path="message" lazy={() => import('./pages/home/pages/message/Message.tsx')} />
       </Route>
-      <Route caseSensitive path="login" lazy={() => import('./pages/login/Login.tsx')} />
-      <Route caseSensitive path="register" lazy={() => import('./pages/register/Register.tsx')} />
     </Route>
   )
 );
