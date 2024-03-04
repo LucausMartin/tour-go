7b8e8210b9feb823e19209645786ba19a39cbd8f
Lucaus <mzx2602685411@gmail.com>
2024-03-04 15:20:00 +0800

    feat(discover-page): ✨ add discover-page article components

--- src/pages/home/pages/discover/Discover.tsx
+++ src/pages/home/pages/discover/Discover.tsx
@@ -8,11 +8,201 @@ import { useWindowSize } from '@uidotdev/usehooks';
 import { useNavigate, useParams, Outlet } from 'react-router-dom';
 import { useMatchLocation } from '@myHooks/useMatchLocation.ts';
 import { MaskDialog } from '@myComponents/MaskDialog/MaskDialog.tsx';
-
+import { LoginPopUps } from '../../../login/Login.tsx';
+import { useSelector } from 'react-redux';
+import { selectLogin } from '@myStore/slices/loginSlice.ts';
+import { FavoriteBorder } from '@mui/icons-material';
 // const a = [...new Array(29).keys()];
 // const webpPath = '/src/assets/img';
 
+// const data = [
+//   {
+//     imgSrc: '/src/assets/img/1.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/2.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/3.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/4.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/5.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/6.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/7.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/8.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/9.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/10.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/11.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/12.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/13.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/14.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/15.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/16.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/17.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/18.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/19.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/20.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/21.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/22.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/23.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/24.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/25.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/26.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/27.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/28.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/29.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/30.webp'
+//   }
+// ];
+
+// const data2 = [
+//   {
+//     imgSrc: '/src/assets/img/31.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/32.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/33.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/34.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/35.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/36.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/37.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/38.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/39.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/40.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/41.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/42.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/43.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/44.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/45.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/46.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/47.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/48.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/49.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/50.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/51.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/52.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/53.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/54.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/55.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/56.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/57.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/58.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/59.webp'
+//   },
+//   {
+//     imgSrc: '/src/assets/img/60.webp'
+//   }
+// ];
+
 const Discover: FC = () => {
+  const loginState = useSelector(selectLogin);
   const windowSize = useWindowSize();
   const [searchPageShow, setSearchPageShow] = useState(false);
   return (
@@ -30,6 +220,7 @@ const Discover: FC = () => {
         </>
       )}
       <Outlet />
+      {loginState === 'ready' ? <LoginPopUps /> : <></>}
     </div>
   );
 };
@@ -162,11 +353,69 @@ const DiscoverTopBar: FC<{ setSearchPageShow: ReactSetState<boolean> }> = ({ set
 };
 
 const DiscoverContent: FC = () => {
-  const param = useParams();
   return (
-    <>
-      <div className="discover-content">{param.kind}</div>
-    </>
+    <div className="discover-content">
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+      <DiscoverContentItem articleID="1" userName="1"></DiscoverContentItem>
+    </div>
   );
 };
 
@@ -183,6 +432,42 @@ const SearchPage: FC<{ searchPageShow: boolean; setSearchPageShow: ReactSetState
     <MaskDialog maskShow={searchPageShow} setMaskShow={setSearchPageShow}>
       <Content />
     </MaskDialog>
+  );
+};
+
+const DiscoverContentItem: FC<{ articleID: string; userName: string }> = ({ articleID, userName }) => {
+  console.log('name', userName);
+  const [imgSrc, setImgSrc] = useState<string>('');
+  const [avatarSrc, setAvatarSrc] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [likeNumber, setLikeNumber] = useState<string>('');
+
+  useEffect(() => {
+    // fetch data
+    setImgSrc(`/src/assets/img/${articleID}.webp`);
+    setAvatarSrc(`/src/assets/avatar.jpg`);
+    setDescription('descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription');
+    setName('其实就是一个用户名，but长一点');
+    setLikeNumber('10');
+  }, [articleID]);
+  return (
+    <div className="discover-content-item">
+      <img src={imgSrc} className="discover-content-item-cover" alt="image" />
+      <div className="discover-content-item-info">
+        <div className="discover-content-item-description">{description}</div>
+        <div className="discover-content-item-article-info">
+          <div className="discover-content-item-article-author">
+            <img src={avatarSrc} className="discover-content-item-article-author-avatar" alt="avatar" />
+            <div className="discover-content-item-article-author-name">{name}</div>
+          </div>
+          <div className="discover-content-item-author-like">
+            <FavoriteBorder className="discover-content-item-author-like-icon" />
+            <div className="discover-content-item-author-like-number">{likeNumber}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
